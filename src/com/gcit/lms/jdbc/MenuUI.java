2ca10d405e0a94cd8c9c758ee2b388308e7e81e7
Krogmer <Krogmer@Kromger>
2018-06-25 10:11:42 -0400

    ReadAllAuthors deleted. Section Menu Loops added to Librarian and borrower.

--- src/com/gcit/lms/jdbc/MenuUI.java
+++ src/com/gcit/lms/jdbc/MenuUI.java
@@ -68,44 +68,82 @@ public class MenuUI {
 		String cardNo = null;
 		BorrowerService bs = new BorrowerService();
 		Borrower user = null;
-		System.out.print("Enter your Card Number: ");
 		while (!valid) {
+			System.out.print("Enter your Card Number: ");
 			cardNo = consoleInput.nextLine();
 			try {
 				user = bs.getBorrower(cardNo);
 				if (user != null)
 					valid = true;
+				else {
+					System.out.print("Invalid ID. ");
+				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		System.out.println("Signed in as: " + user.getName());
-		System.out.printf("\n 1) Check out a book\n 2) Return a book\n 3) Quit to previous\n");
-		selection = consoleInput.nextLine();
-		switch (selection) {
-		case "1":
-			// checkout a book
-			System.out.println("Pick the Branch you want to check out from:");
-			List<Branch> branches = null;
-			try {
-				branches = bs.getAllBranches();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-
-			int i;
-			for (i = 1; i <= branches.size(); i++) {
-				System.out.print((i) + ") ");
-				System.out.println(branches.get(i - 1).getName());
-			}
-			System.out.println(i + ") Quit to previous");
+		valid = true;
+		while (valid) {
+			System.out.printf("\n 1) Check out a book\n 2) Return a book\n 3) Quit to previous\n");
 			selection = consoleInput.nextLine();
-			if (selection.equals(Integer.toString(i))) {
-				return;
-			} else {
-				Branch branchId = branches.get(Integer.parseInt(selection) - 1);
+			int i;
+			switch (selection) {
+			case "1":
+				// checkout a book
+				System.out.println("Pick the Branch you want to check out from:");
+				List<Branch> branches = null;
 				try {
-					List<Book> books = bs.getAllBooksFromBranch(branchId.getId());
+					branches = bs.getAllBranches();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+				for (i = 1; i <= branches.size(); i++) {
+					System.out.print((i) + ") ");
+					System.out.println(branches.get(i - 1).getName());
+				}
+				System.out.println(i + ") Quit to previous");
+				selection = consoleInput.nextLine();
+				if (selection.equals(Integer.toString(i))) {
+					break;
+				} else {
+					Branch branchId = branches.get(Integer.parseInt(selection) - 1);
+					try {
+						List<Book> books = bs.getAllBooksFromBranch(branchId.getId());
+						if (!books.isEmpty() && books != null) {
+							for (i = 1; i <= books.size(); i++) {
+								System.out.print((i) + ") ");
+								System.out.println(books.get(i - 1).getTitle() + " by "
+										+ books.get(i - 1).getAuthors().get(0).getName());
+							}
+							System.out.println(i + ") Quit to cancel operation");
+							selection = consoleInput.nextLine();
+							if (selection.equals(Integer.toString(i))) {
+								break;
+							} else {
+								Book selectedBook = books.get(Integer.parseInt(selection) - 1);
+								bs.checkOutBook(selectedBook, branchId, cardNo);
+
+							}
+						} else {
+							System.out.println("There are not available copies for this branch");
+							break;
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+
+				}
+				break;
+			case "2":
+				// return a book
+				List<Book> books = null;
+				try {
+					books = bs.getAllLoansFromUser(cardNo);
+
 					if (!books.isEmpty() && books != null) {
 						for (i = 1; i <= books.size(); i++) {
 							System.out.print((i) + ") ");
@@ -118,152 +156,128 @@ public class MenuUI {
 							break;
 						} else {
 							Book selectedBook = books.get(Integer.parseInt(selection) - 1);
-							bs.checkOutBook(selectedBook, branchId, cardNo);
-
+							bs.checkInBook(selectedBook, cardNo);
 						}
 					} else {
-						System.out.println("There are not available copies for this branch");
+						System.out.println("You do not have checkout books at this time.");
 						break;
 					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				} catch (ClassNotFoundException e) {
+				} catch (NumberFormatException | ClassNotFoundException | SQLException e) {
 					e.printStackTrace();
 				}
-			}
-			break;
-		case "2":
-			// return a book
-			List<Book> books = null;
-			try {
-				books = bs.getAllLoansFromUser(cardNo);
+				break;
+			case "3":
+				valid = false;
+				break;
+			default:
+				System.out.println("Invalid input");
 
-				if (!books.isEmpty() && books != null) {
-					for (i = 1; i <= books.size(); i++) {
-						System.out.print((i) + ") ");
-						System.out.println(
-								books.get(i - 1).getTitle() + " by " + books.get(i - 1).getAuthors().get(0).getName());
-					}
-					System.out.println(i + ") Quit to cancel operation");
-					selection = consoleInput.nextLine();
-					if (selection.equals(Integer.toString(i))) {
-						break;
-					} else {
-						Book selectedBook = books.get(Integer.parseInt(selection) - 1);
-						bs.checkInBook(selectedBook, cardNo);
-					}
-				} else {
-					System.out.println("You do not have checkout books at this time.");
-					break;
-				}
-			} catch (NumberFormatException | ClassNotFoundException | SQLException e) {
-				e.printStackTrace();
 			}
-			break;
-		case "3":
-			return;
-		default:
-			System.out.println("Invalid input");
-
 		}
 	}
 
 	private static void librarian() {
-		Boolean run = true;
+		Boolean runOutter = true, runInner;
 
-		System.out.printf("\n 1) Enter Branch you manage\n 2) Quit to previous\n");
-		selection = consoleInput.nextLine();
+		while (runOutter) {
+			runInner = true;
+			System.out.printf("\n 1) Enter Branch you manage\n 2) Quit to previous\n");
+			selection = consoleInput.nextLine();
 
-		switch (selection) {
-		case "1":
-			// brach menu
-			LibrarianService ls = new LibrarianService();
-			try {
-				List<Book> booksFromBranch;
-				List<Branch> branches = ls.getAllBranches();
-
-				int i;
-				for (i = 1; i <= branches.size(); i++) {
-					System.out.print((i) + ") ");
-					System.out.println(branches.get(i - 1).getName());
-				}
-				System.out.println(i + ") Quit to previous");
-				selection = consoleInput.nextLine();
-				if (selection.equals(Integer.toString(i))) {
-					return;
-				} else {
-					Integer branchId = branches.get(Integer.parseInt(selection) - 1).getId();
-					System.out.printf(
-							"\n 1) Update the details of the Library\n 2) Add copies to the Branch\n 3) Quite to previous\n");
-					selection = consoleInput.nextLine();
-					switch (selection) {
-					case "1":
-						// Update library
-						System.out.printf(
-								"You have chosen to update the Branch with Branch Id: %d and Branch Name: %s.\n",
-								branchId, branches.get(branchId - 1).getName());
-						System.out.println("Enter ‘quit’ at any prompt to cancel operation.");
-						System.out.println("Please enter new branch name or enter N/A for no change:");
-						String branchName = consoleInput.nextLine();
-						if ("quit".equalsIgnoreCase(branchName)) {
-							break;
-						}
-						System.out.println("Please enter new branch address or enter N/A for no change:");
-						String branchAddress = consoleInput.nextLine();
-						if ("quit".equalsIgnoreCase(branchAddress)) {
-							break;
-						}
-						ls.updateBranchByID(branchId, branchName, branchAddress);
-						System.out.println("Branch successfully updated.");
-						break;
-					case "2":
-						// add copies
-						System.out.println("Pick the Book you want to add copies to your branch:");
-						booksFromBranch = ls.getAllBooksFromBranch(branchId);
-						if (!booksFromBranch.isEmpty() && booksFromBranch != null) {
-							for (i = 1; i <= booksFromBranch.size(); i++) {
-								System.out.print((i) + ") ");
-								System.out.println(booksFromBranch.get(i - 1).getTitle() + " by "
-										+ booksFromBranch.get(i - 1).getAuthors().get(0).getName());
-							}
-							System.out.println(i + ") Quit to cancel operation");
-							selection = consoleInput.nextLine();
-							if (selection.equals(Integer.toString(i))) {
-								break;
-							} else {
-								Integer selectedBook = booksFromBranch.get(Integer.parseInt(selection) - 1).getId();
-								System.out.println(
-										"Existing number of copies: " + ls.getNumberCopiesFrom(selectedBook, branchId));
-								System.out.print("Enter new number of copies: ");
-								ls.updateNoOfCopies(selectedBook, branchId, Integer.parseInt(consoleInput.nextLine()));
-
-							}
-						} else {
-							System.out.println("There are not books for this branch");
-							break;
-						}
-
-						break;
-					case "3":
-						// quit
-						break;
-					default:
-						System.out.println("Wrong input.");
+			switch (selection) {
+			case "1":
+				// brach menu
+				LibrarianService ls = new LibrarianService();
+				try {
+					List<Book> booksFromBranch;
+					List<Branch> branches = ls.getAllBranches();
+					
+					int i;
+					for (i = 1; i <= branches.size(); i++) {
+						System.out.print((i) + ") ");
+						System.out.println(branches.get(i - 1).getName());
 					}
+					System.out.println(i + ") Quit to previous");
+					selection = consoleInput.nextLine();
+					if (selection.equals(Integer.toString(i))) {
+						break;
+					} else {
+						Integer branchId = branches.get(Integer.parseInt(selection) - 1).getId();
+						while (runInner) {
+							System.out.printf(
+									"\n 1) Update the details of the Library\n 2) Add copies to the Branch\n 3) Quite to previous\n");
+							selection = consoleInput.nextLine();
+							switch (selection) {
+							case "1":
+								// Update library
+								System.out.printf(
+										"You have chosen to update the Branch with Branch Id: %d and Branch Name: %s.\n",
+										branchId, branches.get(branchId - 1).getName());
+								System.out.println("Enter ‘quit’ at any prompt to cancel operation.");
+								System.out.println("Please enter new branch name or enter N/A for no change:");
+								String branchName = consoleInput.nextLine();
+								if ("quit".equalsIgnoreCase(branchName)) {
+									break;
+								}
+								System.out.println("Please enter new branch address or enter N/A for no change:");
+								String branchAddress = consoleInput.nextLine();
+								if ("quit".equalsIgnoreCase(branchAddress)) {
+									break;
+								}
+								ls.updateBranchByID(branchId, branchName, branchAddress);
+								System.out.println("Branch successfully updated.");
+								break;
+							case "2":
+								// add copies
+								System.out.println("Pick the Book you want to add copies to your branch:");
+								booksFromBranch = ls.getAllBooksFromBranch(branchId);
+								if (!booksFromBranch.isEmpty() && booksFromBranch != null) {
+									for (i = 1; i <= booksFromBranch.size(); i++) {
+										System.out.print((i) + ") ");
+										System.out.println(booksFromBranch.get(i - 1).getTitle() + " by "
+												+ booksFromBranch.get(i - 1).getAuthors().get(0).getName());
+									}
+									System.out.println(i + ") Quit to cancel operation");
+									selection = consoleInput.nextLine();
+									if (selection.equals(Integer.toString(i))) {
+										break;
+									} else {
+										Integer selectedBook = booksFromBranch.get(Integer.parseInt(selection) - 1)
+												.getId();
+										System.out.println("Existing number of copies: "
+												+ ls.getNumberCopiesFrom(selectedBook, branchId));
+										System.out.print("Enter new number of copies: ");
+										ls.updateNoOfCopies(selectedBook, branchId,
+												Integer.parseInt(consoleInput.nextLine()));
+
+									}
+								} else {
+									System.out.println("There are not books for this branch");
+									break;
+								}
+
+								break;
+							case "3":
+								runInner = false;
+								break;
+							default:
+								System.out.println("Wrong input.");
+							}
+						}
+					}
+				} catch (ClassNotFoundException | SQLException e) {
+					e.printStackTrace();
 				}
+				break;
 
-			} catch (ClassNotFoundException | SQLException e) {
-				e.printStackTrace();
+			case "2":
+				runOutter = false;
+				break;
+			default:
+				System.out.println("Wrong input.");
+
 			}
-			break;
-
-		case "2":
-			return;
-		default:
-			System.out.println("Wrong input.");
-
 		}
-
 	}
 
 	private static void admin() {
